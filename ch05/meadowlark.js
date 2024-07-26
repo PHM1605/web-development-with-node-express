dc84d3c8e1cbf0183a532bc7cd7b72b9f020ff34
RTDevOps <RTDevOps@dmspro.vn>
2024-07-26 17:36:08 +0700

    updates

--- ch05/meadowlark.js
+++ ch05/meadowlark.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const {engine} = require("express-handlebars");
-const fortune = require('../ch03/lib/fortune');
+const fortune = require('./lib/fortune');
+const handlers = require("./lib/handlers");
 
 const app = express();
 app.engine('handlebars', engine({
@@ -11,22 +12,13 @@ app.set('view engine', 'handlebars')
 const port = process.env.PORT || 3000
 app.use(express.static(__dirname + "/public"))
 
-app.get('/', (req, res) => res.render("home"))
+app.get('/', handlers.home);
 
-app.get("/about", (req, res) => {
-  res.render("about", { fortune: fortune.getFortune() })
-})
+app.get("/about", handlers.about);
 
-app.use((req, res) => {
-  res.status(404);
-  res.render("404");
-})
+app.use(handlers.notFound);
 
-app.use((err, req, res, next)=>{
-  console.error(err.message);
-  res.status(500);
-  res.render("500");
-})
+app.use(handlers.serverError);
 
 app.listen(port, () => console.log(
   `Express started on http://localhost:${port}; press Ctrl-C to terminate.`))
