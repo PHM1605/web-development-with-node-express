a8cf03436ece0989c5610a71374bc01caa38b74e
RTDevOps <RTDevOps@dmspro.vn>
2024-08-02 17:42:05 +0700

    updates

---
+++ ch09/lib/handlers.js
@@ -0,0 +1,3 @@
+exports.home = (req, res) => res.render("home");
+exports.notFound = (req, res) => res.render("404");
+exports.serverError = (err, req, res, next) => res.render("500");
---
+++ ch09/meadowlark.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const {engine} = require('express-handlebars');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const expressSession = require("express-session");
+const credentials = require("./config");
+const handlers = require("./lib/handlers");
+const weatherMiddleware = require("./lib/middleware/weather");
+const flashMiddleware = require("./lib/middleware/flash");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// set handlebars for view
+app.engine("handlebars", engine({
+  defaultLayout: "main",
+  helpers: {
+    section: function(name, options) {
+      if (!this._sections) this._sections = {};
+      this._sections[name] = options.fn(this);
+      return null;
+    }
+  }
+}))
+app.set('view engine', 'handlebars');
+
+// turn on body parser
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// use cookie & session
+// NOTE: for session we only work on the "req" object
+app.use(cookieParser(credentials.cookieSecret))
+app.use(expressSession({
+  resave: false,
+  saveUninitialized: false,
+  secret: credentials.cookieSecret
+}))
+app.use(express.static(__dirname + "/public"));
+app.use(weatherMiddleware);
+app.use(flashMiddleware);
+app.get("/", handlers.home);
+
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`Express started on http://localhost:${port}; press Ctrl-C to terminate.`)
+  })
+} else {
+  module.exports = app;
+}
